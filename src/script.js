3f7b8cd5be72e1d3e2a6a7f58005340cad39b797
otanodesignco <otanodesign@gmail.com>
2024-06-03 17:05:57 -0500

    rewrite

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
-import { SphereGeometry, Scene, Mesh, PerspectiveCamera } from 'three'
+import { SphereGeometry, Scene, Mesh, PerspectiveCamera, TextureLoader } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import WebGPU from 'three/addons/capabilities/WebGPU.js'
 import WebGL from 'three/addons/capabilities/WebGL.js'
 import WebGPURenderer from 'three/addons/renderers/webgpu/WebGPURenderer.js'
 import { 
-    MeshBasicNodeMaterial,
+    MeshStandardNodeMaterial,
     cameraPosition,
     positionWorld,
     normalize,
@@ -20,22 +20,23 @@ import {
     vec4,
     mix,
     texture,
-    varying
+    varying,
+    diffuseColor
 } from 'three/examples/jsm/nodes/Nodes.js'
 
 /**
  * Base
  */
 
-const noise = './images/noise.png' // default texture
-const sparkleScale = 1 // scale up noise size
-const sparkleIntensity = 90.0 // intensity of sparkles
-const baseColor = '#101010' // base color
-const fresnelColor = '#e1c564' // fresnel color
-const sparkleColor = '#e1c564' // sparkle color
-const fresenlAmt = 10.0 // amount of fresnel
-const fresnelAlpha = 0.25 // fresnel alpha
-const fresnelIntensity = 5.0 // brightness of fresnel
+const noise = new TextureLoader().load( './images/noise.png' ) // default texture
+const sparkleScale = uniform( 3 ) // scale up noise size
+const sparkleIntensity = uniform( 90.0 ) // intensity of sparkles
+const baseColor = uniform( color( '#800080' ) ) // base color
+const fresnelColor = uniform( color ( '#e1c564' ) ) // fresnel color
+const sparkleColor = uniform( color( '#e1c564' ) ) // sparkle color
+const fresenlAmt = uniform( 5 ) // amount of fresnel
+const fresnelAlpha = uniform( 0.5 ) // fresnel alpha
+const fresnelIntensity = uniform( 1 ) // brightness of fresnel
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -49,54 +50,47 @@ const scene = new Scene()
  * Test mesh
  */
 // Geometry
-const geometry = new SphereGeometry( 1 )
+const geometry = new SphereGeometry( 0.4 )
 
 // Material
  //material
- const material = new MeshBasicNodeMaterial()
- material.colorNode = color('red')
- //uniforms
- const textureScale = uniform( sparkleScale )
- const sparkIntensity = uniform( sparkleIntensity )
- const fresnelFactor = uniform( fresenlAmt )
- const fresnelBrightness = uniform( fresnelIntensity )
- const fresnelAlphaAmt = uniform( fresnelAlpha )
-// view calculations varyings
-const viewDirection = varying( normalize( positionWorld - cameraPosition ) )
-const diffuseView = varying( normalize( cameraPosition - positionWorld ) )
-const normalizedWNormals = varying( normalize( normalWorld ) )
-// sparkle texture
-const sparkleTexture = texture( noise, uv().mul( textureScale ) )
-let sparkleMap = sparkleTexture.rgb
-sparkleMap.sub( 0.5 )
-sparkleMap = normalize( normalize( sparkleMap ) + normalizedWNormals )
+ const material = new MeshStandardNodeMaterial()
+//  //uniforms
 
- // sparkle calculations
- let sparkles = dot( -viewDirection, sparkleMap )
- sparkles = saturate( sparkles )
- sparkles = pow( sparkles, sparkIntensity )
+const uvs = uv()
+const viewDirection = positionWorld.sub( cameraPosition ).normalize()
+const diffuseView = cameraPosition.sub( positionWorld ).normalize()
+const normals = normalWorld.normalize()
 
- //fresnel & diffuse calculation
- let fresnel = dot( normalizedWNormals, viewDirection ) + 1.0
- let diffuse = dot( normalizedWNormals, diffuseView )
- fresnel = pow( fresnel, fresnelFactor )
- diffuse = max( diffuse, 0.0 )
+// texture
+const textureNoise = texture( noise, uvs )
+textureNoise.sub( 0.5 )
+textureNoise.normalize().add( normals )
 
-// color calculations
-const diffuseColor = color( baseColor )
-const rimLightColor = color( fresnelColor )
-const fresnelColorFin = vec4( rimLightColor.r, rimLightColor.b, rimLightColor.g, 1.0 )
-let finalColor = vec4( diffuseColor.r, diffuseColor.b, diffuseColor.g , 1.0 )
-const sparkColor = color( sparkleColor )
-const sparkleColorFin = vec4( sparkColor.r, sparkColor.g, sparkColor.b, 0.0 )
-diffuseColor.mul( diffuse )
-fresnelColorFin.mul( vec4( fresnel, fresnel, fresnel, 1.0 ) )
-fresnelColorFin.rgb.mul( fresnelBrightness )
-sparkleColorFin.rgb.mul( sparkles )
-finalColor.add( sparkleColorFin )
-finalColor = mix( finalColor, fresnelColorFin, fresnel.mul( fresnelAlphaAmt ) )
+// diffuse calculations
+const diffuse = max( normals.dot( diffuseView ), 0.0 )
+// fresnel calculations
+const fresnel = pow( normals.dot( viewDirection ).oneMinus(), fresenlAmt )
+// sparkles
+const sparkles = pow( dot( viewDirection.negate(), textureNoise ).saturate(), sparkleIntensity )
 
-material.colorNode = finalColor
+//Colors
+
+// fresnel Color
+const colorFresnel = vec4( fresnelColor.mul( fresnel ), 1.0 )
+colorFresnel.rgb.mul( fresnelIntensity )
+// diffuse Color
+const colorDiffuse = vec4( baseColor.mul( diffuse ), 1.0 )
+// sparkles color
+const colorSparkles = vec4( sparkleColor.mul( sparkles ), 1.0 )
+
+const colorFinal = mix( colorDiffuse, colorSparkles, sparkles )
+colorFinal.mix( colorFresnel, fresnel )
+
+
+material.colorNode = colorFinal
+
+
 
 // Mesh
 const mesh = new Mesh(geometry, material)
@@ -156,7 +150,7 @@ const tick = () =>
     controls.update()
 
     // Render
-    renderer.renderAsync(scene, camera)
+    renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
